541f30c712860002d7ba2e202317c14aa453f55f
outdoorpet <acooper8@utas.edu.au>
2017-04-10 10:41:02 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+from PyQt4 import QtGui
+from obspy.clients.fdsn import Client
+from obspy.core.event import Catalog
+import pandas as pd
+import os
+
+
+class MainWindow(QtGui.QWidget):
+    """
+    Main Window for metadata map GUI
+    """
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi()
+        self.show()
+        self.raise_()
+
+    def setupUi(self):
+        open_ieb_button = QtGui.QPushButton('Open IEB Output', self)
+        open_ieb_button.released.connect(self.open_ieb)
+        open_ieb_button.resize(open_ieb_button.sizeHint())
+        open_ieb_button.move(50,50)
+
+        self.setGeometry(300, 300, 250, 150)
+
+
+    def open_ieb(self):
+        self.ieb_filename = str(QtGui.QFileDialog.getOpenFileName(
+            parent=self, caption="Choose File",
+            directory=os.path.expanduser("~"),
+            filter="Text Files (*.txt)"))
+        if not self.ieb_filename:
+            return
+
+        # open the text file containing earthquakes with pandas
+        self.ieb_events = pd.read_table(self.ieb_filename, header=0)
+
+        print(self.ieb_events)
+
+        self.make_xml()
+
+    def make_xml(self):
+        client = Client("IRIS")
+        cat = Catalog() # empty earthquake catalogue
+
+        # Method to retrieve events from IRIS based on event ID and create an xml file
+        for event_id in self.ieb_events['IRIS ID']:
+            cat.append(client.get_events(eventid=event_id)[0])
+
+        print(cat)
+        new_filename = os.path.splitext(self.ieb_filename)[0]+'.xml'
+        cat.write(filename=new_filename, format="QUAKEML")
+
+
+
+
+
+if __name__ == '__main__':
+    app = QtGui.QApplication([])
+    w = MainWindow()
+    w.raise_()
+    app.exec_()
